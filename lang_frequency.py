a57e99ec0e4bf8cbde631306831e5862fd6c5d65
Rustymattok <you@example.com>
2019-11-06 19:44:04 +0300

    выполнено задание

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,34 @@
+import re
 
 
 def load_data(filepath):
-    pass
+    if filepath[-4:len(filepath)] != '.txt':  # condition for format txt.
+        print('not correct format')
+        return
+    with open(filepath) as f:
+        text = f.read()
+    return text
 
 
 def get_most_frequent_words(text):
-    pass
+    text = text.lower()
+    match_pattern = re.findall(r'\b[\w]{2,15}\b', text)
+    words = {}
+    for word in match_pattern:
+        count = words.get(word, 0)
+        words[word] = count + 1
+    words_list = list(words.items())
+    # sort words by count max -> min.
+    words_list.sort(key=lambda i: i[1], reverse=True)
+    return words_list  # list words (word:count}) sorted.
 
 
 if __name__ == '__main__':
-    pass
+    file = input('enter file way: ')
+    try:
+        text_file = load_data(file)
+        sorted_list = get_most_frequent_words(text_file)
+        for word in sorted_list[:10]:  # to show only 10 words.
+            print(word[0], word[1])
+    except FileNotFoundError:
+        print("not found file")
