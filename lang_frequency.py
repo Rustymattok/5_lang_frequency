ab96d791ba504f0d57e78a53010df65962e11e6b
Rustymattok <you@example.com>
2019-11-15 01:00:03 +0300

    все операции осуществляет класс Counter,убрал ковычки и поправил методы,поменял названия переменных

--- lang_frequency.py
+++ lang_frequency.py
@@ -8,32 +8,30 @@ def load_data(file_path):
     if not os.path.exists(file_path):
         return None
     with open(file_path, 'r') as file_handler:
-        data_text = file_handler.read()
-    return data_text
+        text_string = file_handler.read()
+    return text_string
 
 
-def get_most_frequent_words(data_text):
-    text = data_text.lower()
+def get_most_frequent_words(text_string):
+    text = text_string.lower()
     match_pattern = re.findall(r'\b[\w]{2,15}\b', text)
-    words_dict = collections.Counter()
-    for word_single in match_pattern:
-        words_dict[word_single] += 1
-    return words_dict
+    words_list = collections.Counter(match_pattern).most_common()
+    return words_list[:10]
 
 
-def get_sorted_list_words(words_dict):
-    words_list = []
-    for word_single in dict(words_dict).items():
-        words_list.append(word_single)
-    words_list.sort(key=lambda i: i[1], reverse=True)
-    return words_list
+def print_words(words_list):
+    for word in words_list:
+        print(*word, sep=' : ')
 
 
 def create_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file',
-                        required=True,
-                        help="command - input file")
+    parser.add_argument(
+        '-f',
+        '--file',
+        required=True,
+        help='command - input file'
+    )
     return parser
 
 
@@ -41,13 +39,11 @@ def main():
     parser = create_parser()
     args = parser.parse_args()
     try:
-        file_data = load_data(args.file)
-        words_dict = get_most_frequent_words(file_data)
-        words_sorted = get_sorted_list_words(words_dict)
-        for word in words_sorted:
-            print(word[0], ": ", word[1])
+        text_string = load_data(args.file)
+        words = get_most_frequent_words(text_string)
+        print_words(words)
     except ValueError:
-        print("not correct format")
+        print('not correct format')
 
 
 if __name__ == '__main__':
