2416cb01f55dc5bf4a8618ff87f6c6f2c7498ab1
Rustymattok <you@example.com>
2019-11-14 08:16:13 +0300

    count word using collection

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,34 +1,54 @@
+import argparse
 import re
+import collections
+import os
 
 
-def load_data(filepath):
-    if filepath[-4:len(filepath)] != '.txt':  # condition for format txt.
-        print('not correct format')
-        return
-    with open(filepath) as f:
-        text = f.read()
-    return text
+def load_data(file_path):
+    if not os.path.exists(file_path):
+        return None
+    with open(file_path, 'r') as file_handler:
+        data_text = file_handler.read()
+    return data_text
 
 
-def get_most_frequent_words(text):
-    text = text.lower()
+def get_most_frequent_words(data_text):
+    text = data_text.lower()
     match_pattern = re.findall(r'\b[\w]{2,15}\b', text)
-    words = {}
-    for word in match_pattern:
-        count = words.get(word, 0)
-        words[word] = count + 1
-    words_list = list(words.items())
-    # sort words by count max -> min.
+    words_dict = collections.Counter()
+    for word_single in match_pattern:
+        words_dict[word_single] += 1
+    return words_dict
+
+
+def get_sorted_list_words(words_dict):
+    words_list = []
+    for word_single in dict(words_dict).items():
+        words_list.append(word_single)
     words_list.sort(key=lambda i: i[1], reverse=True)
-    return words_list  # list words (word:count}) sorted.
+    return words_list
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file',
+                        required=True,
+                        help="command - input file")
+    return parser
+
+
+def main():
+    parser = create_parser()
+    args = parser.parse_args()
+    try:
+        file_data = load_data(args.file)
+        words_dict = get_most_frequent_words(file_data)
+        words_sorted = get_sorted_list_words(words_dict)
+        for word in words_sorted:
+            print(word[0], ": ", word[1])
+    except ValueError:
+        print("not correct format")
 
 
 if __name__ == '__main__':
-    file = input('enter file way: ')
-    try:
-        text_file = load_data(file)
-        sorted_list = get_most_frequent_words(text_file)
-        for word in sorted_list[:10]:  # to show only 10 words.
-            print(word[0], word[1])
-    except FileNotFoundError:
-        print("not found file")
+    main()
